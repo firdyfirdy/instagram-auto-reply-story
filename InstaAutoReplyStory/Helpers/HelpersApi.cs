567e3980c5e47a25122f7fed54edac6aee0ba87e
firdyfirdy <trifirdyanto@gmail.com>
2020-10-08 22:38:17 +0700

    First Commit

---
+++ InstaAutoReplyStory/Helpers/HelpersApi.cs
@@ -0,0 +1,23 @@
+﻿using InstagramApiSharp.API;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InstaAutoReplyStory.Helpers
+{
+  public static class HelpersApi
+  {
+    public static IInstaApi InstaApi { get; set; }
+    
+    public static void WriteLine(string value, ConsoleColor color = ConsoleColor.DarkGreen)
+    {
+      //
+      // This method writes an entire line to the console with the string.
+      //
+      Console.ForegroundColor = color;
+      Console.Write(value);
+      Console.ResetColor();
+      Console.WriteLine();
+    }
+  }
+}
---
+++ InstaAutoReplyStory/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InstaAutoReplyStory
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
